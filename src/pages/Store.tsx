52120872a89fbdc1aa5abbcf6828248c6c61466a
yoshino9397 <yosino8512@gmail.com>
2022-09-07 15:49:33 -0700

    store page DONE

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,5 +1,6 @@
 import { items } from "../data/items.js";
 import { Col, Row } from "react-bootstrap";
+import StoreItem from "../components/StoreItem";
 
 const Store = () => {
   return (
@@ -8,8 +9,7 @@ const Store = () => {
       <Row md={2} xs={1} lg={3} className="g-3">
         {items.map((item) => (
           <Col key={item.id}>
-            <h2>{item.name}</h2>
-            {/* <img src={item.imgUrl} alt="" /> */}
+            <StoreItem {...item} />
           </Col>
         ))}
       </Row>
