924049f1a2d2d871e3390778fbdae0b958a78f64
yoshino9397 <yosino8512@gmail.com>
2022-09-06 23:44:17 -0700

    store now

---
+++ src/data/items.js
@@ -0,0 +1,26 @@
+export const items = [
+  {
+    id: 1,
+    name: "Book",
+    price: 10.99,
+    imgUrl: "https://live.staticflickr.com/6132/6043817389_cf6ace99c0_3k.jpg",
+  },
+  {
+    id: 2,
+    name: "Candle",
+    price: 1199,
+    imgUrl: "https://live.staticflickr.com/3876/14951775332_3e650111ac_k.jpg",
+  },
+  {
+    id: 3,
+    name: "Mug",
+    price: 1.05,
+    imgUrl: "https://live.staticflickr.com/4028/4379024698_806ead15f4_k.jpg",
+  },
+  {
+    id: 4,
+    name: "Diary Note",
+    price: 14000,
+    imgUrl: "https://live.staticflickr.com/4121/4751807057_f72a6864ef_z.jpg",
+  },
+];
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,5 +1,20 @@
+import { items } from "../data/items.js";
+import { Col, Row } from "react-bootstrap";
+
 const Store = () => {
-  return <h1>Store</h1>;
+  return (
+    <>
+      <h1>Store</h1>
+      <Row md={2} xs={1} lg={3} className="g-3">
+        {items.map((item) => (
+          <Col key={item.id}>
+            <h2>{item.name}</h2>
+            {/* <img src={item.imgUrl} alt="" /> */}
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
 };
 
 export default Store;
